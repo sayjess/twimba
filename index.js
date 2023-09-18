79b752fe3aad743bf344080032ed799e30edc980
Jess <jessjelardo@gmail.com>
2023-09-18 18:47:43 +0800

    adding image assets

---
+++ index.js
@@ -0,0 +1,7 @@
+import { tweetsData } from './data.js'
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetInput.value)
+})
