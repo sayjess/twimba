b77653b60cdc162c11106b9693866fbae4b95f57
Jess <jessjelardo@gmail.com>
2023-10-29 05:38:42 +0800

    added sign in page

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyDpcqazNO7BG0wRO9vkMZ2S4buaeMAmFTM",
+    authDomain: "twimba-6ff84.firebaseapp.com",
+    projectId: "twimba-6ff84",
+    storageBucket: "twimba-6ff84.appspot.com",
+  };
+
+  const app = initializeApp(firebaseConfig)
